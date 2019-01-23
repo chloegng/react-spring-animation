ff69a5cd126d95a304b6acc3ee47537e78fcfcd5
Chloe <chloegng@gmail.com>
2019-01-22 18:37:32 -0800

    add toggle button for component 3

--- react-spring/src/components/Component2.js
+++ react-spring/src/components/Component2.js
@@ -14,6 +14,7 @@ export default class Component2 extends Component {
           <div style={c2Style}>
            <h1>Component 2</h1>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa maxime recusandae soluta rem eum magni magnam placeat alias, minus quaerat nulla. Quasi, sed! Eum rem non nisi fugiat, debitis ea.</p>
+            <button style={btn} onClick={this.props.toggle}>Toggle Component 3</button>
           </div>
         </div>
       )}
@@ -26,4 +27,13 @@ const c2Style = {
   background: 'slateblue',
   color: 'white',
   padding: '1.5rem',
+}
+
+const btn = {
+  background: '#333',
+  color: 'white',
+  padding: '1rem 2rem',
+  border: 'none',
+  textTransform: 'uppercase',
+  margin: '15px 0'
 }
---
+++ react-spring/src/components/Component3.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Component3() {
+  return (
+    <div style={c3Style}>
+      <h1>Component 3</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus doloremque omnis, nesciunt ea voluptates aperiam alias! Quas, veritatis laudantium modi sequi ullam libero voluptas dicta necessitatibus quia velit fugit, eveniet consectetur. Voluptatibus corporis pariatur non nesciunt magnam porro consequuntur ipsa.</p>
+    </div>
+  )
+}
+
+const c3Style = {
+  background: 'powderblue',
+  color: 'white',
+  padding: '1.5rem',
+}
