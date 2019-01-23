4b7ee709e438d8518c9dea04b5b8e88f3012a30f
Chloe <chloegng@gmail.com>
2019-01-22 18:02:44 -0800

    remove boiler plate and add component

---
+++ react-spring/src/components/Component1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Component1() {
+  return (
+    <div>
+      <h1>Component 1</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa maxime recusandae soluta rem eum magni magnam placeat alias, minus quaerat nulla. Quasi, sed! Eum rem non nisi fugiat, debitis ea.</p>
+    </div>
+  )
+}
