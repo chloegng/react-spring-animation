83426734473eea095de8abc841d123057aeb7634
Chloe <chloegng@gmail.com>
2019-01-22 18:26:07 -0800

    create components, add spring animation, and counter

--- react-spring/src/components/Component1.js
+++ react-spring/src/components/Component1.js
@@ -1,10 +1,48 @@
 import React from 'react'
+import {Spring} from 'react-spring'
 
 export default function Component1() {
   return (
-    <div>
-      <h1>Component 1</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa maxime recusandae soluta rem eum magni magnam placeat alias, minus quaerat nulla. Quasi, sed! Eum rem non nisi fugiat, debitis ea.</p>
-    </div>
+    <Spring
+      from={{ opacity: 0, marginTop: -500 }}
+      to={{ opacity: 1, marginTop: 0 }}
+
+    >
+      { props => (
+        <div style={props}>
+          <div style={c1Style}>
+           <h1>Component 1</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa maxime recusandae soluta rem eum magni magnam placeat alias, minus quaerat nulla. Quasi, sed! Eum rem non nisi fugiat, debitis ea.</p>
+            <Spring
+              from={{ number: 0 }}
+              to={{ number: 10 }}
+              config={{ duration: 10000 }}
+            >
+              {props => (
+                <div style={props}>
+                  <h1 style={counter}>
+                    {props.number.toFixed()}
+                  </h1>
+                </div>
+              )}
+            </Spring>
+          </div>
+        </div>
+      )}
+    </Spring>
   )
 }
+
+const c1Style = {
+  background: 'steelblue',
+  color: 'white',
+  padding: '1.5rem',
+}
+
+const counter = {
+  background: '#333',
+  textAlign: 'center',
+  width: '100px',
+  borderRadius: '50%',
+  margin: '1rem auto'
+};
---
+++ react-spring/src/components/Component2.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import {Spring} from 'react-spring'
+
+export default class Component2 extends Component {
+  render() {
+    return (
+      <Spring
+      from={{ opacity: 0}}
+      to={{ opacity: 1 }}
+      config={{ delay: 1000, duration: 1000 }}
+    >
+      { props => (
+        <div style={props}>
+          <div style={c2Style}>
+           <h1>Component 2</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa maxime recusandae soluta rem eum magni magnam placeat alias, minus quaerat nulla. Quasi, sed! Eum rem non nisi fugiat, debitis ea.</p>
+          </div>
+        </div>
+      )}
+    </Spring>
+    )
+  }
+}
+
+const c2Style = {
+  background: 'slateblue',
+  color: 'white',
+  padding: '1.5rem',
+}
